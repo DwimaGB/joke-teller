6b663492df980a4abdbe56bac655051f13ab246a
Dwima Basumatary <dwima1583@gmail.com>
2023-04-29 10:47:53 +0530

    functionality complete

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -1,19 +1,22 @@
 
 const btn = document.getElementById('joke-btn');
 const audioElement = document.getElementById('audio');
+const replayBtn = document.getElementById('replay');
 const jokeDisplay = document.getElementById('joke');
 
 const serverApi = `${window.origin}/api`;
 
 
 btn.addEventListener('click', async () => {
+    btn.disabled = true; // disabling button to avoid simultaneous request
+
     try {
         const tssData = await requestTextToSpeech();
 
         const audioData = `data:audio/mp3;base64,${tssData.audioData}`;
 
         audioElement.src = audioData;
-
+        audioElement.play();
     }
     catch (e) {
         console.log(e);
@@ -21,16 +24,35 @@ btn.addEventListener('click', async () => {
     }
 })
 
+audioElement.addEventListener('loadedmetadata', ()=>{
+    setTimeout(()=>{
+        btn.disabled = false;
+    }, audioElement.duration * 1000)  // enabling again after the joke has been told
+    
+})
+
+replayBtn.addEventListener('click', ()=>{
+    if(audioElement.duration > 0){ // just ensuring audio has been loaded
+        audioElement.play();
+    }
+})
 
 /*  */
 
 async function getJoke() {
-    const jokeUri = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single'
+    const jokeUri = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit'
 
     try {
         const jokeResponse = await fetch(jokeUri); // fetching joke from joke api
 
-        return jokeResponse.json();
+        const jokeData = await jokeResponse.json();
+
+        if(!jokeData.joke){
+            return `${jokeData.setup}...${jokeData.delivery}`;
+        }
+        else{
+            return jokeData.joke;
+        }
     }
     catch (e) {
         console.log(e);
@@ -40,15 +62,14 @@ async function getJoke() {
 
 async function requestTextToSpeech() {
     try {
-        const jokeData = await getJoke();
-        const joke = jokeData.joke;
+        const joke = await getJoke();
 
         jokeDisplay.innerText = joke; // display the joke
 
         const options = { method: 'POST', headers: { 'content-type': 'application/json' }, body: JSON.stringify({ joke }) }; // sending the joke to the server side to request for text to speech api
 
         const tssResponse = await fetch(serverApi, options);
-        const tssData = await tssResponse.json(); // getting the audioData send by the server side using TSS api
+        const tssData = await tssResponse.json(); // getting the audioData responsed by the server side using TSS api
 
         if (tssResponse.status === 500) {
             throw new Error(tssData.message);
